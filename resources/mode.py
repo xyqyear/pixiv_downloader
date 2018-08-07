dbbf78f22c26bd3036b2a9082e9930bc9809fcea
xyqyear <215908558@qq.com>
2018-08-07 13:17:42 +0800

    清除掉了一些不能用的模式

--- resources/mode.py
+++ resources/mode.py
@@ -60,27 +60,25 @@ class ModeSwitcher:
                 break
             print('请输入正确的日期格式\n')
 
-        modes = {'0': 'day', '1': 'day_original', '2': 'day_rookie',
-                 '3': 'week', '4': 'week_original', '5': 'week_rookie',
-                 '6': 'month', '7': 'month_original', '8': 'month_rookie',
+        modes = {'0': 'day','3': 'week','6': 'month',
+                 '7': 'week_rookie', '8': 'week_original',
                  '9': 'day_male', '10': 'day_female',
                  '11': 'day_manga', '12': 'week_manga', '13': 'month_manga',
 
-                 '14': 'day_r18', '15': 'week_r18', '16': 'month_r18',
+                 '14': 'day_r18', '15': 'week_r18',
                  '17': 'day_male_r18', '18': 'day_female_r18',
-                 '19': 'day_r18_manga', '20': 'week_r18_manga', '21': 'month_r18_manga'}
+                 '19': 'day_r18_manga', '20': 'week_r18_manga'}
         while True:
             mode_digit = input('''
 请输入下载什么榜单，对应表如下:
-        0：日榜       1：日榜，首次上榜  2：日榜，曾上过榜
-        3：周榜       4：周榜，首次上榜  5：周榜，曾上过榜
-        6：月榜       7：月榜，首次上榜  8：月榜，曾上过榜
-        9：日榜，男性向                 10：日榜，女性向
-        11：日榜漫画  12：周榜漫画       13：月榜漫画
+        0：日榜       3：周榜        6：月榜
+        7:新人 周榜                  8:原创 周榜
+        9：日榜，男性向              10：日榜，女性向
+        11：日榜漫画  12：周榜漫画    13：月榜漫画
 
-        14：日榜r18     15：周榜r18     16：月榜r18
-        17：日榜男性向r18               18：日榜女性向r18
-        19：日榜r18漫画 20：周榜r18漫画 21：月榜r18漫画
+        14：日榜r18                  15：周榜r18     
+        17：日榜男性向r18            18：日榜女性向r18
+        19：日榜r18漫画              20：周榜r18漫画
 请输入一个数字:''')
             if mode_digit in modes:
                 mode = modes[mode_digit]
@@ -92,4 +90,4 @@ class ModeSwitcher:
 
     def mode4(self):
         import json
-        print(json.dumps(self.api_object.illust_ranking()))
+        print(json.dumps(self.api_object.illust_ranking(mode='day_female_r18')))
