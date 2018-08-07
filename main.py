3be1db3ce630c303b7082dc660894a9b59cf5d81
TeemoKill <TeemoKill@users.noreply.github.com>
2018-08-07 13:04:01 +0800

    some small changes

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from resources.managers import UserManager
 from resources.mode import ModeSwitcher
 
 
-
 if __name__ == '__main__':
     aapi = AppPixivAPI()
-    user_manager = UserManager()
+    user_manager = UserManager(aapi)
     mode_switcher = ModeSwitcher(aapi)
 
-    user_manager.login(aapi)
+    user_manager.login()
     mode_switcher.start()
--- resources/managers.py
+++ resources/managers.py
@@ -12,37 +12,52 @@ token_file = "token.tkn"
 
 class UserManager:
 
-    def login(self, api_object):
-        token_holder = TokenHolder()
+    def __init__(self, api_object):
+        self.token_holder = TokenHolder()
+        self.api_object = api_object
+
+    def login(self):
+        commands = {"1": self.login_with_token,
+                    "2": self.login_with_password}
         if os.path.exists(token_file):
-            print("您登陆过，正在验证...")
-            stored_tokens = token_holder.tokens
-            tokens = token_holder.auth(api_object,
+            print("您登陆过，是否使用保存的通行证登录？")
+            print("(本地仅保存登录token，我们不会保存您的密码)")
+            while True:
+                command = input("1：使用保存的通行证登录 2：重新登录\n")
+                if command in commands:
+                    commands[command]()
+                    break
+                else:
+                    print("请输入正确的指令")
+        else:
+            self.login_with_password()
+
+    def login_with_token(self):
+        stored_tokens = self.token_holder.tokens
+        tokens = self.token_holder.auth(self.api_object,
                                        access_token=stored_tokens['access_token'],
                                        refresh_token=stored_tokens['refresh_token'])
-            if tokens:
-                print('登陆成功！')
-                token_holder.tokens = tokens
-
-            else:
-                print('旧的登录信息已经失效，请重新用用户名密码登录')
-                os.remove(token_file)
-                self.login(api_object)
-
+        if tokens:
+            print('登陆成功！')
+            self.token_holder.tokens = tokens
         else:
-            print('请输入用户名(或邮箱地址)和密码来登录')
-            username = input('请输入用户名(或邮箱地址): ')
-            password = getpass.getpass('请输入密码: ')
-            tokens = token_holder.auth(api_object,
+            print('旧的登录信息已经失效，请重新用用户名密码登录')
+            os.remove(token_file)
+            self.login_with_password()
+
+    def login_with_password(self):
+        print('请输入用户名(或邮箱地址)和密码来登录')
+        username = input('请输入用户名(或邮箱地址): ')
+        password = getpass.getpass('请输入密码: ')
+        tokens = self.token_holder.auth(self.api_object,
                                        username=username,
                                        password=password)
-            if tokens:
-                print('登陆成功!')
-                token_holder.tokens = tokens
-
-            else:
-                print('用户名/密码错误或网络状况不好，请重新登录!')
-                self.login(api_object)
+        if tokens:
+            print('登陆成功!')
+            self.token_holder.tokens = tokens
+        else:
+            print('用户名/密码错误或网络状况不好，请重新登录!')
+            self.login_with_password()
 
 
 class TokenHolder:
